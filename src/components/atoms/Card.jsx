58fbeba48b5b0757d9320533af07eecf565e9111
Alessandro Battiato <alessandroDev@outlook.it>
2023-04-18 19:45:33 +0200

    feat: input UI completed

---
+++ src/components/atoms/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import cat from "../../cat.PNG";
+
+const Card = () => {
+  return (
+    <div>
+      <figure>
+        <img src={cat} />
+      </figure>
+    </div>
+  );
+};
+
+export default Card;
